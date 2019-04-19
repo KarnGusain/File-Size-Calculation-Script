4ac29d70e7cec5d165a1ab9c5bf8790b6e7c09c7
Karn Gusain <rockypy17@gmail.com>
2019-04-19 15:42:31 +0530

    Create File_sizeCal_awk.sh

---
+++ File_sizeCal_awk.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# File_sizeCal_awk.sh
+FILE="/tmp/logs.txt"
+FIND=$(/bin/find /data1 -xdev -size +10M -exec ls -aGl {} \; >> "$FILE")
+echo ""
+awk '$7 >= 2002 && $7 <= 2018 {
+  sum[$3] += $4
+}
+
+END {
+
+  for (i in sum) {
+    printf "User %-22s Total Space consumed: %3.2f GB\n", i, sum[i]/2**30
+    total += sum[i]
+  }
+  print "==========================================================="
+  printf "Total Space consumed by All Users: %3.2f GB\n", total/ 2**30
+  echo ""
+
+}' $FILE
+
+/usr/bin/unlink "$FILE"
