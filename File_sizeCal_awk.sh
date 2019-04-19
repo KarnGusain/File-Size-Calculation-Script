95ae2e443e98cba80ea8b5f8cc2eafda99f775bc
Karn Gusain <rockypy17@gmail.com>
2019-04-19 16:07:31 +0530

    Update File_sizeCal_awk.sh

--- File_sizeCal_awk.sh
+++ File_sizeCal_awk.sh
@@ -2,7 +2,7 @@
 # File_sizeCal_awk.sh
 # File Path
 FILE="/tmp/logs.txt"
-# Find command to list the files above than 10MB size 
+# Find command to list the files above than 10MB size which excluded group.
 FIND=$(/bin/find /data1 -xdev -size +10M -exec ls -aGl {} \; >> "$FILE")
 echo ""
 awk '$7 >= 2002 && $7 <= 2018 {
