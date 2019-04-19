d7fb148c03b09a19ed695c8d36ecb8e6a040bbff
Karn Gusain <rockypy17@gmail.com>
2019-04-19 23:09:36 +0530

    Update FileSize_per_User1.sh

--- FileSize_per_User1.sh
+++ FileSize_per_User1.sh
@@ -1,9 +1,15 @@
+# This is small awk snippet which takes the Files "$FILE" variable and "$FILE" gets the feed from find command 
+# where it looks the file Size more than 1MB and list them with the all the information except the group.
+# However, It sorts the file owner and based on that aggregats the size consumed by that particulat user 
+# and displays that along and at the end summrises the total size consumed by all the users for the mount.
+# Please visit the readme section for output Result example.
+# This is Just another variation of the script "FileSize_per_User.sh" , this just incorporates a little add on feature
+# to look for the Files specific between two Years, in this case ie ->  between 2002 - 2018 
+
 #!/bin/sh
-# File_sizeCal_awk.sh
 # File Path
 FILE="/tmp/logs.txt"
-# Find command to list the files above than 10MB size which excluded group.
-/bin/find /data1 -xdev -size +10M -exec ls -aGl {} \; >> "$FILE"
+/bin/find /data1 -xdev -size +1M -exec ls -aGl {} \; >> "$FILE"
 echo ""
 awk '$7 >= 2002 && $7 <= 2018 {
   sum[$3] += $4
